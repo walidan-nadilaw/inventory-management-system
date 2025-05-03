535f41eb681437a0c08fac905ad2e75d35fc7bf7
fxkhrii <afiffakhri04@gmail.com>
2025-05-03 12:39:28 +0700

    Menambahkan resource controller untuk Item

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('items', ItemController::class)->only([
+    'index', 'store', 'update', 'destroy'
+]);
