6383d25b14a899bbc6bd51910882af43b493d7b2
walidan-nadilaw <sirsnakeson@gmail.com>
2025-05-08 19:10:51 +0700

    fix inventory.php
    use hasfactory
    change column names

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factory\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
 
 class Inventory extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['item', 'category', 'quantity', 'price'];
+
 }
