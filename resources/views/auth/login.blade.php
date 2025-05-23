2fe346a26e48b37fe06069926ec3cc4140a38017
Brilyan Pancar Komara <bripankom@gmail.com>
2025-05-24 01:21:20 +0700

    bug fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -69,7 +69,7 @@
 
             <div>
                 <button type="submit"
-                        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition ease-in-out duration-150">
+                        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition ease-in-out duration-150">
                     Masuk
                 </button>
             </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,7 @@
             </div>
             <ul class="space-y-1 px-3">
                 <li>
-                    <a href="/home" class="flex items-center px-4 py-3 rounded-md hover:bg-gray-600 transition-colors text-gray-300 hover:text-white {{ request()->is('home') ? 'bg-primary text-white' : '' }}">
+                    <a href="{{ route('home') }}" class="flex items-center px-4 py-3 rounded-md hover:bg-gray-600 transition-colors text-gray-300 hover:text-white {{ request()->is('/') ? 'bg-primary text-white' : '' }}">
                         <i class="fas fa-home mr-3"></i> Home
                     </a>
                 </li>
@@ -47,13 +47,8 @@
                     </a>
                 </li>
                 <li>
-                    <a href="/reports" class="flex items-center px-4 py-3 rounded-md hover:bg-gray-600 transition-colors text-gray-300 hover:text-white">
-                        <i class="fas fa-chart-bar mr-3"></i> Reports
-                    </a>
-                </li>
-                <li>
-                    <a href="/settings" class="flex items-center px-4 py-3 rounded-md hover:bg-gray-600 transition-colors text-gray-300 hover:text-white">
-                        <i class="fas fa-cog mr-3"></i> Settings
+                    <a href="/logout" class="flex items-center px-4 py-3 rounded-md hover:bg-gray-600 transition-colors text-gray-300 hover:text-white">
+                        <i class="fas fa-arrow-right-from-bracket mr-3"></i> Logout
                     </a>
                 </li>
             </ul>
