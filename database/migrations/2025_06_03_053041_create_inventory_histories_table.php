ac4c987fee97e5fc87aea18cda2ab18e56991ec2
walidan-nadilaw <sirsnakeson@gmail.com>
2025-06-03 12:52:35 +0700

    create history table

---
+++ database/migrations/2025_06_03_053041_create_inventory_histories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inventory_histories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('inventory_id')->constrained('inventories')->onDelete('cascade');
+            $table->foreignId('user_id')->nullable()->constrained('users')->nullable()->onDelete('set null');
+            $table->enum('action', ['added', 'reduced', 'update product'])->default('reduced');
+            $table->integer('old_quantity')->nullable();
+            $table->integer('new_quantity')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inventory_histories');
+    }
+};
